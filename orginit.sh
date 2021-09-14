4ca14a62bfb253da61852241efcad9a602bfdebb
dschach <dschach@x2od.com>
2021-09-14 15:55:14 -0700

    Orginit update to order of operations

--- orginit.sh
+++ orginit.sh
@@ -4,10 +4,10 @@ sfdx force:source:push
 
 sfdx force:user:permset:assign -n Action_Plans_Admin
 
-sfdx force:org:open
-
 # To install additional sample Accounts/Contacts
 # sfdx force:data:tree:import -p ./data/action-plan-data-plan.json
 
 # To install sample action plan template
 # sfdx force:apex:execute -f ./data/sample-data.apex
+
+sfdx force:org:open
