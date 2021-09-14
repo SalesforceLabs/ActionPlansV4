f4d74984e51128788ee329a03764b521b9515b68
dschach <dschach@x2od.com>
2021-09-13 18:50:07 -0700

    Iniital commit, updated for namespace
    Reworked some code to allow for a namespace in a managed package
    Ported all code from pull request on ActionPlans v3
    This is the start of ActionPlans v4 in its new repository
    Deployed to main, as cannot fork and branches had separate histories

---
+++ orginit.sh
@@ -0,0 +1,13 @@
+sfdx force:org:create -f config/project-scratch-def.json -a ActionPlans -s
+
+sfdx force:source:push
+
+sfdx force:user:permset:assign -n Action_Plans_Admin
+
+sfdx force:org:open
+
+# To install additional sample Accounts/Contacts
+# sfdx force:data:tree:import -p ./data/action-plan-data-plan.json
+
+# To install sample action plan template
+# sfdx force:apex:execute -f ./data/sample-data.apex
