9fbea74e0c71b8c53ceba4a76416ad1139eb0e92
David Schach <dschach@x2od.com>
2022-02-18 23:33:21 -0800

    Managed vs Unmanaged Project Config (#13)
    * DML Errors Fixes
    
    * Metadata cleanup
    
    * Project config

---
+++ scripts/orginit-managed.sh
@@ -0,0 +1,35 @@
+echo "Cleaning previous scratch org..."
+sfdx force:org:delete -p -u ActionPlans
+
+echo "Enterprise Edition"
+sed -i "" "s|\"edition\": \"Developer\"|\"edition\": \"Enterprise\"|" config/project-scratch-def.json
+
+echo "Using namespace"
+sed -i "" "s|\"namespace\": \"\"|\"namespace\": \"LabsActionPlans\"|" sfdx-project.json
+
+echo "Creating new scratch org"
+sfdx force:org:create -f config/project-scratch-def.json -a ActionPlans -s
+
+# For use with namespaced scratch org in package development process
+echo "Pushing metadata"
+sfdx force:source:push
+
+echo "Assigning permission set"
+sfdx force:user:permset:assign -n Action_Plans_Admin
+
+# To install sample action plan template
+echo "Loading sample data"
+sfdx force:apex:execute -f ./data/sample-data.apex
+
+echo "Clearing namespace"
+sed -i "" "s|\"namespace\": \"LabsActionPlans\"|\"namespace\": \"\"|" sfdx-project.json
+
+echo "Developer Edition"
+sed -i "" "s|\"edition\": \"Enterprise\"|\"edition\": \"Developer\"|" config/project-scratch-def.json
+
+# To install sample Flow and other metadata
+echo "Installing sample flow"
+sfdx force:source:deploy -p sfdx-source/unmanagedExtension
+
+echo "opening org"
+sfdx force:org:open
