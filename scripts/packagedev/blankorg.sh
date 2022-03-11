1858a1c9ae96294c16fbfa180c495e223016b873
David Schach <dschach@x2od.com>
2022-03-10 22:40:40 -0800

    Script is cleaner

--- scripts/packagedev/blankorg.sh
+++ scripts/packagedev/blankorg.sh
@@ -1,5 +1,8 @@
 # BLANK scratch org
 
+echo "Delete old blank org"
+sfdx force:org:delete -p -u PackageInstallTest
+
 echo "Creating new scratch org"
 sfdx force:org:create --definitionfile config/enterprise-scratch-def.json --setalias PackageInstallTest --nonamespace --durationdays 7 --noancestors
 
