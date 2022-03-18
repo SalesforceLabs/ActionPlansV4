afd008adb2f753dbe5742e9027ea70121b169b9d
David Schach <dschach@x2od.com>
2022-03-17 20:19:50 -0700

    Start package versioning via release

--- scripts/packagedev/build.sh
+++ scripts/packagedev/build.sh
@@ -1,7 +1,7 @@
 echo "set managed package"
 sed -i "" "s|\"namespace\": \"\"|\"namespace\": \"LabsActionPlans\"|" sfdx-project.json
 
-sfdx force:package:version:create --codecoverage --package ActionPlans --path sfdx-source/LabsActionPlans --installationkeybypass --wait 20 --skipancestorcheck
+sfdx force:package:version:create --codecoverage --package ActionPlans --path sfdx-source/LabsActionPlans --installationkeybypass --wait 20
 
 echo "unset namespace"
 sed -i "" "s|\"namespace\": \"LabsActionPlans\"|\"namespace\": \"\"|" sfdx-project.json
