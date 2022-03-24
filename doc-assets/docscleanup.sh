63a0cec4568c93874706f066f7bba99bf5a42e68
David Schach <dschach@x2od.com>
2022-03-23 23:41:30 -0700

    Highlight to 11.5, VF code fix

--- doc-assets/docscleanup.sh
+++ doc-assets/docscleanup.sh
@@ -2,3 +2,4 @@
 # find docs -type f -name '*.html' -exec sed -i '' 's|<h3 class=\"subsection-title \\([^/]*\\)</h2>|<h3 class=\"subsection-title \\1</h3>|g' {} +
 
 sed -i '' 's|<h3 class="subsection-title\([^/]*\)</h2>|<h3 class="subsection-title\1</h3>|g' docs/*.html
+sed -i '' 's|<br/>|\n|g' docs/*.html
