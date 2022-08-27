f84e714fe7a5e6e365bc77f9d9892b819a66cc61
David Schach <dschach@x2od.com>
2022-08-27 16:58:47 -0700

    Cosmetic rearrangements, apexdoc (#55)

--- scripts/updateHighlight.sh
+++ scripts/updateHighlight.sh
@@ -3,3 +3,5 @@
 curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/highlight.min.js" -o "doc-assets/highlight.js"
 echo >> "doc-assets/highlight.js"
 curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex@1/dist/apex.min.js" >> "doc-assets/highlight.js"
+
+git add doc-assets/highlight.js
