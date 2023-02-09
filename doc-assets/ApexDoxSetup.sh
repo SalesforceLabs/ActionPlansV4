178e1ce30ff4bcf6505429fcf809e7ec742f08c3
David Schach <dschach@x2od.com>
2023-02-08 22:08:34 -0800

    feat(docs): ApexDox version upgrade

---
+++ doc-assets/ApexDoxSetup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+printf '<link href="assets/styling.css" rel="stylesheet" />' > "doc-assets/README.html"
+echo >> "doc-assets/README.html"
+npx marked -i README.md --breaks >> "doc-assets/README.html"
+
+sed -i '' 's|href="./|target="_blank" href="https://github.com/SalesforceLabs/ActionPlansV4/tree/main/|g' doc-assets/README.html
+
+sed -i '' 's|src="sfdx-source/LabsActionPlans/main/default/staticresources/ActionPlan_Resources/about_images|class="readmeimage" src="assets|g' doc-assets/README.html
+
+printf '<link href="assets/styling.css" rel="stylesheet" />' > "doc-assets/INSTALLATION.html"
+echo >> "doc-assets/INSTALLATION.html"
+npx marked -i INSTALLATION.md --breaks >> "doc-assets/INSTALLATION.html"
