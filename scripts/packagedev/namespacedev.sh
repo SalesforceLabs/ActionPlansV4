70f43ba924930914499b9ad3cdc8d5dc0c9796be
David Schach <dschach@x2od.com>
2023-03-05 15:17:45 -0800

    fix: page redirect on template from object

--- scripts/packagedev/namespacedev.sh
+++ scripts/packagedev/namespacedev.sh
@@ -7,7 +7,8 @@ echo "Using namespace"
 sed -i "" "s|\"namespace\": \"\"|\"namespace\": \"LabsActionPlans\"|" sfdx-project.json
 
 echo "Creating new scratch org"
-sf force org create --definitionfile config/project-scratch-def.json --setalias ActionPlans --setdefaultusername --noancestors --durationdays 21
+#sf force org create --definitionfile config/project-scratch-def.json --setalias ActionPlans --setdefaultusername --noancestors --durationdays 21
+sf org create scratch --definition-file config/project-scratch-def.json --alias ActionPlans --set-default --no-ancestors --duration-days 21
 
 # For use with namespaced scratch org in package development process
 echo "Pushing managed metadata"
@@ -18,10 +19,11 @@ sf deploy metadata  --source-dir sfdx-source/unmanaged
 
 echo "Assigning permission set"
 sf org assign permset --name Action_Plans_Admin
-
+#
 # To install sample action plan template
 echo "Loading sample data"
 sfdx force:apex:execute -f ./data/sample-data.apex
+#sf apex run --apex-code-file "data/sample-data.apex" —-target-org ActionPlans
 
 echo "Clearing namespace"
 sed -i "" "s|\"namespace\": \"LabsActionPlans\"|\"namespace\": \"\"|" sfdx-project.json
