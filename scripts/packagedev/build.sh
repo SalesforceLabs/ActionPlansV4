0f4465b7f0e812620594874efb5cdfe571e51f45
David Schach <dschach@x2od.com>
2023-03-10 21:19:33 -0800

    build(pkg): remove redundancy in version create

--- scripts/packagedev/build.sh
+++ scripts/packagedev/build.sh
@@ -4,7 +4,7 @@ echo "set managed package"
 sed -i "" "s|\"namespace\": \"\"|\"namespace\": \"LabsActionPlans\"|" sfdx-project.json
 
 #sf package version create --code-coverage --path sfdx-source/LabsActionPlans --installation-key-bypass --wait 20
-sfdx force:package:version:create --codecoverage --package "Action Plans" --path sfdx-source/LabsActionPlans --installationkeybypass --wait 20
+sfdx force:package:version:create --codecoverage --path sfdx-source/LabsActionPlans --installationkeybypass --wait 20
 
 echo "unset namespace"
 sed -i "" "s|\"namespace\": \"LabsActionPlans\"|\"namespace\": \"\"|" sfdx-project.json
